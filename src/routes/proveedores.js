0f8b829ee2a4094475de4f1789ecd5cdc2fae9b3
Puzzless <simon01ruiz@gmail.com>
2024-02-17 10:12:55 -0500

    ruta protegida proveedores

--- src/routes/proveedores.js
+++ src/routes/proveedores.js
@@ -4,14 +4,14 @@ const verificarToken = require('../middlewares/verificarToken');
 
 
 const { getProveedor,getProveedores, postProveedor, putProveedor, deleteProveedor, getProveedorProductos, cambiarEstadoProveedor } = require('../controllers/proveedores');
-route.get('/proveedores', getProveedores);
 
-route.get('/proveedores', getProveedores);
-route.get('/proveedores/:id', getProveedor);
-route.get('/proveedores/productos/:id', getProveedorProductos);
-route.post('/proveedores', postProveedor);
-route.put('/proveedores/:id', putProveedor);
-route.put('/proveedores/:id/cambiarestado')
-route.delete('/proveedores/:id', deleteProveedor);
+route.get('/proveedores',verificarToken, getProveedores);
+route.get('/proveedores',verificarToken, getProveedores);
+route.get('/proveedores/:id',verificarToken, getProveedor);
+route.get('/proveedores/productos/:id',verificarToken, getProveedorProductos);
+route.post('/proveedores',verificarToken, postProveedor);
+route.put('/proveedores/:id',verificarToken, putProveedor);
+route.put('/proveedores/:id/cambiarestado', cambiarEstadoProveedor);
+route.delete('/proveedores/:id', verificarToken, deleteProveedor);
 
 module.exports = route;
